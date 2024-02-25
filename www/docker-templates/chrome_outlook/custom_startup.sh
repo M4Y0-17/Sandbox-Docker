2f6629930d959d3ade0d6147fafba5033856900c
Alex Mayo <35707527+M4Y0-17@users.noreply.github.com>
2024-02-25 13:34:13 +0100

    Delete www directory

--- www/docker-templates/chrome_outlook/custom_startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Inicia Chrome con un User-Agent personalizado
-google-chrome --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36" "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=19&ct=1703625675&rver=7.0.6738.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fcobrandid%3dab0455a0-8d03-46b9-b18b-df2f57b9e44c%26nlp%3d1%26RpsCsrfState%3de35d36a7-fb90-efb0-f925-4d35f3ce0773&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=ab0455a0-8d03-46b9-b18b-df2f57b9e44c"
--- www/utils/sheduler.py
+++
@@ -1,9 +0,0 @@
-from utils.docker import delete_old_containers
-from apscheduler.schedulers.background import BackgroundScheduler
-
-
-
-def start_delete_old_dockers_scheduler(app):
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(func=lambda: delete_old_containers(app), trigger="interval", minutes=1)
-    scheduler.start()
